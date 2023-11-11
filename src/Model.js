487a22a9802ab0f7ab103490ffe8abd589df7081
swJaNG12 <cbdjej38483@gmail.com>
2023-11-11 18:49:41 +0900

    refactor: centralize error throwing in Model
    - Add #throwInvalidDateOfVisitError method to handle error throwing
    - Replace direct error throwing with #throwInvalidDateOfVisitError
    in validation method

--- src/Model.js
+++ src/Model.js
@@ -7,20 +7,24 @@ export class Model {
 		this.#validateVisitDateRange(dateOfVisit);
 	}
 
+	#throwInvalidDateOfVisitError() {
+		throw new Error(ErrorMessage.invalidDateOfVisit());
+	}
+
 	#parseStringToNumber(string) {
 		return parseInt(string, DECIMAL_NUMBER);
 	}
 
 	#validateVisitDateIsNumber(dateOfVisit) {
 		if (!PATTERN.ONLY_NUMBER.test(dateOfVisit)) {
-			throw new Error(ErrorMessage.invalidDateOfVisit());
+			this.#throwInvalidDateOfVisitError();
 		}
 	}
 
 	#validateVisitDateRange(dateOfVisit) {
 		const visitDate = this.#parseStringToNumber(dateOfVisit);
 		if (visitDate < EVENT.DATE_RANGE.MIN || visitDate > EVENT.DATE_RANGE.MAX) {
-			throw new Error(ErrorMessage.invalidDateOfVisit());
+			this.#throwInvalidDateOfVisitError();
 		}
 	}
 }
